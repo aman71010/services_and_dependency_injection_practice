bcdc7c93e392b6bf719f3abd519cd07764f4f479
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-12 17:05:18 +0530

    inject service in another service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,19 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { LoggingService } from '../logging.services';
 import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css'],
-  providers: [ LoggingService ]
+  styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
+  constructor(private accountsService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChanged(accountStatus);
   }
 }
